63eaf51e19988d348b09045847c15f91b3ec81fc
sbkitchpanich3 <bankster007@gmail.com>
2017-10-06 02:39:45 -0800

    Initial commit

---
+++ app/src/main/java/com/sbkitchpanich/kitchpanichcsce305project1part7/MainActivity.java
@@ -0,0 +1,84 @@
+package com.sbkitchpanich.kitchpanichcsce305project1part7;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText filterText;
+
+    private ArrayAdapter<String> listAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        filterText = (EditText)findViewById(R.id.editText);
+
+        ListView itemList = (ListView)findViewById(R.id.listView);
+
+        String [] listViewAdapterContent = {"appointment - นัด", "bird - นก", "building - ตึก", "chest - อก", "child - เด็ก",
+                                            "ripe - สุก", "six - หก", "small - เล็ก", "to bite - กัด", "to blow - พัด",
+                                            "to cut - ตัด", "to love - รัก", "to recall - นึก", "to rest - พัก", "to scrub - ขัด",
+                                            "to think - คิด", "to turn off - ปิด", "vegetable - ผัก", "wig - วิก", "wrong - ผิด"};
+
+        listAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1, listViewAdapterContent);
+
+        itemList.setAdapter(listAdapter);
+
+        itemList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+// make Toast when click
+                Toast.makeText(getApplicationContext(), "Position " + position, Toast.LENGTH_LONG).show();
+            }
+        });
+        filterText.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                MainActivity.this.listAdapter.getFilter().filter(s);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+    }
+
+    /* deprecated?
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+// Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+// Handle action bar item clicks here. The action bar will
+// automatically handle clicks on the Home/Up button, so long
+// as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+//noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    */
+}
